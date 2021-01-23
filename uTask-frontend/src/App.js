c20426934ac33687fc2e93929c9445db20af7f7e
Renato771 <renato771@tutanota.com>
2021-01-23 17:42:41 -0300

    Adicionado funcionalidade das notas

--- uTask-frontend/src/App.js
+++ uTask-frontend/src/App.js
@@ -8,15 +8,27 @@ import beach from "./assets/beach.png";
 
 function App() {
     const [tasks, setTasks] = useState([]);
+    const [tasksDone, setTasksDone] = useState([]);
 
     const [tarefa_input , setTarefa_input] = useState('');
 
     useEffect(() => {
         async function loadTasks() {
+            function equalsTrue(entry){
+                return entry.done === true; 
+            }
+
+            function equalsFalse(entry){
+                return entry.done === false;
+            }
+
             const response = await api.get("/items/read");
         
-            setTasks(response.data);
-            console.log(tasks)
+            setTasks((response.data).filter(equalsTrue));
+            setTasksDone((response.data).filter(equalsFalse));
+
+            console.log(tasks);
+            console.log(tasksDone);
         }
 
         loadTasks();
@@ -33,6 +45,8 @@ function App() {
 
         console.log(response.data);
         setTarefa_input("");
+
+        setTasks([...tasks, response.data]);
     }
 
     return ( 
@@ -41,13 +55,13 @@ function App() {
                 <h1 className="center">uTask</h1>
 
                 <button id="refresh" >refresh!</button> {/* temporário */}
-            
+
                 <form className="center" onSubmit={handleSubmit} >
                     <input 
-                    type="text" 
+                    type="text"
                     required 
                     placeholder="Add uma nova tarefa"
-                    pattern="[.]{1,48}"
+                    pattern=".{1,48}"
                     title="Entrada deve ter no máximo 48 caracteres"
                     value={tarefa_input}
                     onChange={e => setTarefa_input(e.target.value)} >
@@ -66,35 +80,21 @@ function App() {
                     {/*  {tasks.map(task => (
                         <p>{tasks.id}</p>
                     ))};    */}
+                    {tasks.map(task => (
+                        <div key={task._id} className="center task" >
+                            <div className="taskLeft" >
+                                <p>{task.content}</p>
+                            </div>
+        
+                            <div className="taskRight" >
+                                <button className="taskGreen" alt="Marcar tarefa como concluída" ></button>
+                                <button className="taskRed" alt="Deletar tarefa" ></button>
+                            </div>
+                        </div>
+                    ))}
                 </div>
 
                 {/*
-
-                Não feita
-
-                <div className="center task" >
-                    <div className="taskLeft" >
-                        <p>C o m p r r r r r r r r r r r a a a a a a a a aa</p>
-                    </div>
-
-                    <div className="taskRight" >
-                        <button className="taskGreen" alt="" ></button>
-                        <button className="taskRed" alt="" ></button>
-                    </div>
-                </div>
-
-                Feita
-
-                <div className="center task taskD" >
-                    <div className="taskLeft" >
-                        <p><s>C  r r a a a a a aa</s></p>
-                    </div>
-
-                    <div className="taskRight" >
-                        <button></button>
-                    </div>
-                </div>
-
                 Nada (praia)
                 
                 <div id="nada" className="center" >
@@ -111,6 +111,17 @@ function App() {
                 <h2 className="center" >DONE</h2>
 
                 <div id="div-rodape" >
+                    {tasksDone.map(task => (
+                        <div key={task._id} className="center task taskD" >
+                            <div className="taskLeft" >
+                                <p><s>{task.content}</s></p>
+                            </div>
+        
+                            <div className="taskRight" >
+                                <button  ></button>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </section>
         </div>
