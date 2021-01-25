3183c07c85155eb0ee20b35b267b51e8cda53163
Renato771 <renato771@tutanota.com>
2021-01-25 15:37:14 -0300

    Organizacao do CSS

--- uTask-frontend/src/index.js
+++ uTask-frontend/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import './CSS/index.css';
+import './CSS/sectionTop.css';
+import './CSS/tasks.css';
+import './CSS/sectionMiddle.css';
+import './CSS/sectionBottom.css';
+
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
